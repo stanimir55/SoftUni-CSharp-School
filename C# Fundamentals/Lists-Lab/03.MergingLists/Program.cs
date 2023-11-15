b5bbe8875af15ffd0a2a10c497fac0eb832040f2
Stanimir <miro_2000s@abv.bg>
2023-11-15 20:55:13 +0200

    Added

---
+++ C# Fundamentals/Lists-Lab/03.MergingLists/Program.cs
@@ -0,0 +1,30 @@
+﻿List<int> first = ReadIntList();
+
+List<int> second = ReadIntList();
+
+List<int> result = new List<int>();
+
+int interrations = Math.Max(first.Count, second.Count);
+
+for (int i = 0; i < interrations; i++)
+{
+    if (i < first.Count)
+    {
+        result.Add(first[i]);
+    }
+
+    if (i < second.Count)
+    {
+        result.Add(second[i]);
+    }
+}
+
+Console.WriteLine(string.Join(" ", result));
+
+static List<int> ReadIntList()
+{
+    return Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+}
