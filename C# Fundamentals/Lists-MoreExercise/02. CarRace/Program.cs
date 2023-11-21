27a19e6453315d7a244be9015132ab06be86fa22
Stanimir <miro_2000s@abv.bg>
2023-11-21 15:59:25 +0200

    Added

---
+++ C# Fundamentals/Lists-MoreExercise/02. CarRace/Program.cs
@@ -0,0 +1,35 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+double leftRacerTime = 0;
+double rightRacerTime = 0;
+
+for (int i = 0; i < numbers.Count / 2; i++)
+{
+    leftRacerTime += numbers[i];
+
+    if (numbers[i] == 0)
+    {
+        leftRacerTime *= 0.80;
+    }
+}
+for (int i = numbers.Count - 1; i > numbers.Count / 2; i--)
+{
+    rightRacerTime += numbers[i];
+
+    if (numbers[i] == 0)
+    {
+        rightRacerTime *= 0.80;
+    }
+}
+
+if (leftRacerTime < rightRacerTime)
+{
+    Console.WriteLine($"The winner is left with total time: {leftRacerTime:#.#}");
+}
+else
+{
+    Console.WriteLine($"The winner is right with total time: {rightRacerTime}");
+}
+ 
---
+++ C# Fundamentals/Lists-MoreExercise/04.MixedUpLists/Program.cs
@@ -0,0 +1,43 @@
+﻿List<int> inputOne = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+List<int> inputTwo = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+List<int> result = new List<int>();
+List<int> output = new List<int>();
+
+if (inputOne.Count > inputTwo.Count)
+{
+    result.Add(inputOne[inputOne.Count - 1]);
+    result.Add(inputOne[inputOne.Count - 2]);
+    inputOne.RemoveAt(inputOne.Count - 1);
+    inputOne.RemoveAt(inputOne.Count- 1);
+}
+else if (inputTwo.Count > inputOne.Count)
+{
+    result.Add(inputTwo[0]);
+    result.Add(inputTwo[1]);
+    inputTwo.RemoveAt(0);
+    inputTwo.RemoveAt(0);
+}
+
+inputTwo.Reverse();
+
+for (int i = 0; i < inputOne.Count; i++)
+{
+    output.Add(inputOne[i]);
+    output.Add(inputTwo[i]);
+}
+
+result.Sort();
+output.Sort();
+
+output.RemoveAll(x => x <= result[0]);
+output.RemoveAll(x => x >= result[1]);
+
+Console.WriteLine(string.Join(" ",output));
