dfc238ed00cbcde05a02525b04afbf2bbe260ba2
Stanimir <miro_2000s@abv.bg>
2023-11-23 12:51:56 +0200

    Added

---
+++ C# Fundamentals/Programming-Fundamentals-Mid-Exam/01 ProgrammingFundamentalsMidExamRetake/03.MemoryGame/Program.cs
@@ -0,0 +1,55 @@
+﻿List<string> elements = Console.ReadLine()
+    .Split()
+    .ToList();
+
+string command;
+int counter = 0;
+
+while ((command = Console.ReadLine()) != "end")
+{
+    counter++;
+
+    List<string> indexes = command.Split().ToList();
+
+    int indexOne = int.Parse(indexes[0]);
+    int indexTwo = int.Parse(indexes[1]);
+
+    if (indexOne >= 0 && indexTwo >= 0 && indexOne < elements.Count && indexTwo < elements.Count && indexOne != indexTwo)
+    {
+        if (elements[indexOne] == elements[indexTwo])
+        {
+            Console.WriteLine($"Congrats! You have found matching elements - {elements[indexOne]}!");
+
+            if (indexOne > indexTwo)
+            {
+                elements.RemoveAt(indexOne);
+                elements.RemoveAt(indexTwo);
+            }
+            else
+            {
+                elements.RemoveAt(indexTwo);
+                elements.RemoveAt(indexOne);
+            }
+
+            if (elements.Count == 0)
+            {
+                Console.WriteLine($"You have won in {counter} turns!");
+                return;
+            }
+        }
+        else if (elements[indexOne] != elements[indexTwo])
+        {
+            Console.WriteLine("Try again!");
+        }
+    }
+    else
+    {
+        elements.Insert(elements.Count / 2, $"-{counter}a");
+        elements.Insert(elements.Count / 2, $"-{counter}a");
+
+        Console.WriteLine("Invalid input! Adding additional elements to the board");
+    }
+}
+
+Console.WriteLine("Sorry you lose :(");
+Console.WriteLine(string.Join(" ", elements));
