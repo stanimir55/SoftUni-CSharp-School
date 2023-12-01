86f3130e4040de1a5284fea77584bf971937bf63
Stanimir <miro_2000s@abv.bg>
2023-12-01 16:37:50 +0200

    Added

---
+++ C# Fundamentals/ObjectsAndClasses-MoreExercise/01.CompanyRoster/Program.cs
@@ -0,0 +1,58 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+List<Employee> employees1 = new List<Employee>();
+List<string> departmens = new List<string>();
+
+for (int i = 0; i < number; i++)
+{
+    string[] employees = Console.ReadLine()
+        .Split()
+        .ToArray();
+
+    Employee employee = new Employee();
+
+    string name = employees[0];
+    double salary = double.Parse(employees[1]);
+    string department = employees[2];
+
+    employee.Name = name;
+    employee.Salary = salary;
+    employee.Department = department;
+
+    employees1.Add(employee);
+    departmens.Add(department);
+}
+
+employees1 = employees1.OrderBy(x => x.Department).ToList();
+departmens = departmens.Distinct().ToList();
+
+string departmentHighestAve = "";
+double highestAveSalary = double.MinValue;
+
+foreach (string department in departmens)
+{
+    double aveSolary = employees1.Where(e => e.Department == department).Select(e => e.Salary).Average();
+
+    if (aveSolary > highestAveSalary)
+    {
+        departmentHighestAve = department;
+        highestAveSalary = aveSolary;
+    }
+}
+
+Console.WriteLine($"Highest Average Salary: {departmentHighestAve}");
+
+foreach (var employee in employees1.Where(e => e.Department == departmentHighestAve).OrderByDescending(e => e.Salary))
+{
+    Console.WriteLine($"{employee.Name} {employee.Salary:F2}");
+}
+
+
+class Employee
+{
+    public string Name { get; set; }
+
+    public double Salary { get; set; }
+
+    public string Department { get; set; }
+}
---
+++ C# Fundamentals/ObjectsAndClasses-MoreExercise/03.SpeedRacing/Program.cs
@@ -0,0 +1,76 @@
+﻿int number = int.Parse(Console.ReadLine());
+List<Car> cars = new List<Car>();
+
+for (int i = 0; i < number; i++)
+{
+    string[] arguments = Console.ReadLine()
+        .Split()
+        .ToArray();
+
+    string model = arguments[0];
+    double fuelAmout = double.Parse(arguments[1]);
+    double fuelConsumption = double.Parse(arguments[2]);
+
+    Car car = new Car();
+
+    car.Model = model;
+    car.FuelAmout = fuelAmout;
+    car.FuelConsumption = fuelConsumption;
+    
+    cars.Add(car);
+}
+
+string command;
+
+while ((command = Console.ReadLine()) != "End")
+{
+    string[] arguments = command.Split();
+
+    string commandDrive = arguments[0];
+    string model = arguments[1];
+    int amoutOfKm = int.Parse(arguments[2]);
+
+    for (int i = 0; i < cars.Count; i++)
+    {
+        if (cars[i].Model == model)
+        {
+            cars[i].Calculate(amoutOfKm);
+        }
+    }
+}
+
+for (int i = 0; i < cars.Count; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+
+
+class Car
+{
+    public string Model { get; set; }
+
+    public double FuelAmout { get; set; }
+
+    public double FuelConsumption { get; set; }
+
+    public int TraveledKm { get; set; }
+
+    public void Calculate(int currentDistance)
+    {
+        double amoutOFuel = FuelConsumption * currentDistance;
+        if (FuelAmout >= amoutOFuel)
+        {
+            FuelAmout -= amoutOFuel;
+            TraveledKm += currentDistance;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Model} {FuelAmout:F2} {TraveledKm}";
+    }
+}
