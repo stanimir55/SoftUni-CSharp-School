8f2012f2a7295fd525c81f303ce0ae468e2bca97
Stanimir <miro_2000s@abv.bg>
2023-12-02 17:50:37 +0200

    Added

---
+++ C# Fundamentals/AssociativeArrays-Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var numberCurrences = new SortedDictionary<int, int>();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int currentNumber = numbers[i];
+
+    if (!numberCurrences.ContainsKey(numbers[i]))
+    {
+        numberCurrences[currentNumber] = 1;
+    }
+    else
+    {
+        numberCurrences[currentNumber]++;
+    }
+}
+
+foreach (KeyValuePair<int,int> kvp in numberCurrences)
+{
+    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+}
---
+++ C# Fundamentals/AssociativeArrays-Lab/04.WordFilter/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+List<string> words = Console.ReadLine()
+    .Split()
+    .Where(w => w.Length % 2 == 0)
+    .ToList();
+
+Console.WriteLine(string.Join(Environment.NewLine, words));
