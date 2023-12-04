f59ef0cd20c57fcbf90579a906754ab9f796a5b6
Stanimir <miro_2000s@abv.bg>
2023-12-04 14:10:19 +0200

    Added

---
+++ C# Fundamentals/AssociativeArrays-MoreExercise/04.Snowwhite/Program.cs
@@ -0,0 +1,67 @@
+﻿string command;
+var dwarfs = new Dictionary<string, Dictionary<string, int>>();
+
+while ((command = Console.ReadLine()) != "Once upon a time")
+{
+    string[] arguments = command
+        .Split(" <:> ")
+        .ToArray();
+
+    string dwarfName = arguments[0];
+    string dwarfHatColor = arguments[1];
+    int dwarfPhysics = int.Parse(arguments[2]);
+
+    if (!dwarfs.ContainsKey(dwarfHatColor))
+    {
+        dwarfs[dwarfHatColor] = new Dictionary<string, int>();
+        dwarfs[dwarfHatColor].Add(dwarfName, dwarfPhysics);
+    }
+    else
+    {
+        bool haveDwardWithTheSameName = false;
+
+        foreach (var kvp in dwarfs[dwarfHatColor])
+        {
+            string currentName = kvp.Key;
+            int currentPhysics = kvp.Value;
+
+            if (currentName == dwarfName)
+            {
+                haveDwardWithTheSameName = true;
+
+                if (dwarfPhysics > currentPhysics)
+                {
+                    dwarfs[dwarfHatColor][dwarfName] = dwarfPhysics;
+                    break;
+                }
+            }
+        }
+
+        if (haveDwardWithTheSameName == false)
+        {
+            dwarfs[dwarfHatColor].Add(dwarfName, dwarfPhysics);
+        }
+    }
+}
+
+var sortedDwarfs = new Dictionary<string, int>();
+
+foreach (var kvp in dwarfs.OrderByDescending(x => x.Value.Count()))
+{
+    string hatColor = kvp.Key;
+    var nameAndPhysics = kvp.Value;
+
+    foreach (var kvpOne in nameAndPhysics)
+    {
+        string name = kvpOne.Key;
+        int physics = kvpOne.Value;
+        sortedDwarfs.Add($"({hatColor}) {name} <-> ", physics);
+    }
+}
+foreach (var kvp in sortedDwarfs.OrderByDescending(x => x.Value))
+{
+    string name = kvp.Key;
+    int physycs = kvp.Value;
+
+    Console.WriteLine($"{name}{physycs}");
+}
---
+++ C# Fundamentals/AssociativeArrays-MoreExercise/05.DragonArmy/Program.cs
@@ -0,0 +1,64 @@
+﻿int drgonsCount = int.Parse(Console.ReadLine());
+var dragons = new Dictionary<string, Dictionary<string, List<double>>>();
+
+for (int i = 0; i < drgonsCount; i++)
+{
+    string[] arguments = Console.ReadLine()
+        .Split()
+        .ToArray();
+
+    string type = arguments[0];
+    string name = arguments[1];
+
+    double defaultDamage = 45;
+    double defaultHealth = 250;
+    double defaultArmor = 10;
+
+    double newDamage;
+    double newHealth;
+    double newArmor;
+
+    if (!dragons.ContainsKey(type))
+    {
+        dragons.Add(type, new Dictionary<string, List<double>>());
+    }
+    if (!dragons[type].ContainsKey(name))
+    {
+        dragons[type].Add(name, new List<double>());
+        dragons[type][name].Add(double.TryParse(arguments[2], out newDamage) ? newDamage : defaultDamage);
+        dragons[type][name].Add(double.TryParse(arguments[3], out newHealth) ? newHealth : defaultHealth);
+        dragons[type][name].Add(double.TryParse(arguments[4], out newArmor) ? newArmor : defaultArmor);
+    }
+    else
+    {
+        dragons[type][name].Clear();
+        dragons[type][name].Add(double.TryParse(arguments[2], out newDamage) ? newDamage : defaultDamage);
+        dragons[type][name].Add(double.TryParse(arguments[3], out newHealth) ? newHealth : defaultHealth);
+        dragons[type][name].Add(double.TryParse(arguments[4], out newArmor) ? newArmor : defaultArmor);
+    }
+}
+
+foreach (var type in dragons)
+{
+    double averageDamage = 0;
+    double averageHealth = 0;
+    double averageArmor = 0;
+    double count = 0;
+
+    foreach (var stats in type.Value)
+    {
+        List<double> dragonStats = stats.Value;
+        averageDamage += dragonStats[0];
+        averageHealth += dragonStats[1];
+        averageArmor += dragonStats[2];
+        count++;
+    }
+
+    Console.WriteLine($"{type.Key}::({averageDamage/count:F2}/{averageHealth/count:F2}/{averageArmor/count:F2})");
+
+    foreach (var dragon in type.Value.OrderBy(x => x.Key))
+    {
+        List<double> dragonInfo = dragon.Value;
+        Console.WriteLine($"-{dragon.Key} -> damage: {dragonInfo[0]}, health: {dragonInfo[1]}, armor: {dragonInfo[2]}");
+    }
+}
