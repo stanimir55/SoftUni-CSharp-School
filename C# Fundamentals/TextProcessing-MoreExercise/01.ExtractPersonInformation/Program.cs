142d59ce39992a52b3530e1e50a132018cc64a47
Stanimir <miro_2000s@abv.bg>
2023-12-07 13:17:50 +0200

    Added

---
+++ C# Fundamentals/TextProcessing-MoreExercise/01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,18 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string strings = Console.ReadLine();
+
+    int indexOfNameStar = strings.IndexOf('@');
+    int indexOfNameEnd = strings.IndexOf('|');
+
+    string name = strings.Substring(indexOfNameStar + 1, indexOfNameEnd - indexOfNameStar - 1);
+
+    int indexOfAgeStart = strings.IndexOf('#');
+    int indexOfAgeEnd = strings.IndexOf('*');
+
+    string age = strings.Substring(indexOfAgeStart + 1, indexOfAgeEnd - indexOfAgeStart - 1);
+
+    Console.WriteLine($"{name} is {age} years old.");
+}
---
+++ C# Fundamentals/TextProcessing-MoreExercise/05.HTML/Program.cs
@@ -0,0 +1,18 @@
+﻿string titleOfAnArticle = Console.ReadLine();
+string contentArticle = Console.ReadLine();
+string command;
+
+Console.WriteLine("<h1>");
+Console.WriteLine($"   {titleOfAnArticle}");
+Console.WriteLine("</h1>");
+Console.WriteLine("<article>");
+Console.WriteLine($"   {contentArticle}");
+Console.WriteLine("</article>");
+
+while ((command = Console.ReadLine()) != "end of comments")
+{
+    Console.WriteLine("<div>");
+    Console.WriteLine($"   {command}");
+    Console.WriteLine("</div>");
+}
+
