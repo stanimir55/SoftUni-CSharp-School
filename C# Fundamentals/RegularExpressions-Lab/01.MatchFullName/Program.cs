0c441142e0bebd0082e222940f32cd019840b705
Stanimir <miro_2000s@abv.bg>
2023-12-08 17:15:34 +0200

    Added

---
+++ C# Fundamentals/RegularExpressions-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)"; 
+
+Regex regex = new Regex(pattern); 
+MatchCollection matches = regex.Matches(input); 
+
+foreach (Match match in matches)
+{
+    Console.Write($"{match.Groups[0]} "); 
+}
---
+++ C# Fundamentals/RegularExpressions-Lab/03.MatchDates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+string pattern = @"(?<day>\d{2})([\.|\-|\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(input);
+
+foreach (Match match in matches)
+{
+    //var day = match.Groups["day"].Value;
+    //var month = match.Groups["month"].Value;
+    //var year = match.Groups["year"].Value;
+    Console.WriteLine($"Day: {match.Groups["day"].Value}, Month: {match.Groups["month"].Value}, Year: {match.Groups["year"].Value}");
+    //Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+}
