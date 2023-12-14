9d3c55f24d95301ce78d6ba81334287d29ffd818
Stanimir <miro_2000s@abv.bg>
2023-12-14 16:20:22 +0200

    Added

---
+++ C# Fundamentals/Programming-Fundamentals-Final-Exam/03.ProgrammingFundamentalsFinalExamRetake/03.NeedForSpeedIII/Program.cs
@@ -0,0 +1,89 @@
+﻿int numberOfCars = int.Parse(Console.ReadLine());
+
+var carsDistance = new Dictionary<string, int>();
+var carsFuel = new Dictionary<string, int>();
+
+for (int i = 0; i < numberOfCars; i++)
+{
+    string[] car = Console.ReadLine()
+        .Split('|')
+        .ToArray();
+
+    string carName = car[0];
+    int carMileage = int.Parse(car[1]);
+    int fuel = int.Parse(car[2]);
+
+    carsDistance[carName] = carMileage;
+    carsFuel[carName] = fuel;
+}
+
+string[] command = Console.ReadLine().Split(" : ").ToArray();
+
+while (command[0] != "Stop")
+{
+    if (command[0] == "Drive")
+    {
+        int amortizationMileage = 100000;
+        int distance = int.Parse(command[2]);
+        int fuel = int.Parse(command[3]);
+
+        if (fuel > carsFuel[command[1]])
+        {
+            Console.WriteLine("Not enough fuel to make that ride");
+        }
+        else
+        {
+            carsDistance[command[1]] += distance;
+            carsFuel[command[1]] -= fuel;
+
+            Console.WriteLine($"{command[1]} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+        }
+
+        if (carsDistance[command[1]] >= amortizationMileage)
+        {
+            Console.WriteLine($"Time to sell the {command[1]}!");
+
+            carsDistance.Remove(command[1]);
+            carsFuel.Remove(command[1]);
+        }
+    }
+    else if (command[0] == "Refuel")
+    {
+        int maxRefillLevel = 75;
+        int fuel = int.Parse(command[2]);
+        int actualNeededFuelAmount = maxRefillLevel - carsFuel[command[1]];
+
+        carsFuel[command[1]] += fuel;
+
+        if (carsFuel[command[1]] > maxRefillLevel)
+        {
+            Console.WriteLine($"{command[1]} refueled with {actualNeededFuelAmount} liters");
+            carsFuel[command[1]] = maxRefillLevel;
+        }
+        else
+        {
+            Console.WriteLine($"{command[1]} refueled with {fuel} liters");
+        }
+    }
+    else if (command[0] == "Revert")
+    {
+        int minimumMilegeLevel = 10000;
+        int revert = int.Parse(command[2]);
+
+        carsDistance[command[1]] -= revert;
+
+        if (carsDistance[command[1]] >= minimumMilegeLevel)
+        {
+            Console.WriteLine($"{command[1]} mileage decreased by {revert} kilometers");
+        }
+        else
+        {
+            carsDistance[command[1]] = minimumMilegeLevel;
+        }
+    }
+
+    command = Console.ReadLine().Split(" : ").ToArray();
+}
+
+Console.WriteLine(string.Join($"{Environment.NewLine}", carsDistance
+    .Select(x => $"{x.Key} -> Mileage: {x.Value} kms, Fuel in the tank: {carsFuel[x.Key]} lt.")));
---
+++ C# Fundamentals/Programming-Fundamentals-Final-Exam/04.ProgrammingFundamentalsFinalExam/01.PasswordReset/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+string password = Console.ReadLine();
+string commands;
+
+StringBuilder newPassword = new StringBuilder();
+
+while ((commands = Console.ReadLine()) != "Done")
+{
+    string[] command = commands
+        .Split();
+
+    switch (command[0])
+    {
+        case "TakeOdd":
+
+            for (int i = 1; i < password.Length; i += 2)
+            {
+                newPassword.Append(password[i]);
+            }
+
+            password = newPassword.ToString();
+            Console.WriteLine(password);
+            break;
+        case "Cut":
+            int index = int.Parse(command[1]);
+            int length = int.Parse(command[2]);
+
+            password = password.Remove(index, length);
+
+            Console.WriteLine(password);
+            break;
+        case "Substitute":
+
+            if (password.Contains(command[1]))
+            {
+                password = password.Replace(command[1], command[2]);
+
+                Console.WriteLine(password);
+            }
+            else
+            {
+                Console.WriteLine("Nothing to replace!");
+            }
+
+            break;
+    }
+}
+
+Console.WriteLine($"Your password is: {password}");
