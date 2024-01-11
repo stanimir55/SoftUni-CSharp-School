72b42fcd2361d213511a1bf361d659efb35c906c
Stanimir <miro_2000s@abv.bg>
2024-01-11 12:03:07 +0200

    Added

---
+++ C# Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,7 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    int sum = i * n;
+    Console.WriteLine($"{n} X {i} = {sum}");
+}
---
+++ C# Fundamentals/BasicSyntax,ConditionalStatementsandLoops-Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+int k = int.Parse(Console.ReadLine());
+int total = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    if (k >= 1)
+    {
+        total = n * k;
+        Console.WriteLine($"{n} X {k++} = {total}");
+        if (k > 10)
+        {
+            break;
+        }
+    }
+    if (k > 10)
+    {
+        total = n * k;
+        Console.WriteLine($"{n} X {k} = {total}");
+        break;
+    }
+}
