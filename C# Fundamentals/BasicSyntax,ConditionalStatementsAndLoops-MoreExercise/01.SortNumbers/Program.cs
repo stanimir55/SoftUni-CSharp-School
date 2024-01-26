09d3800eaf7fc5b8b697f31dc167b5bf23e9131f
Stanimir <miro_2000s@abv.bg>
2024-01-26 22:26:20 +0200

    Added

---
+++ C# Fundamentals/BasicSyntax,ConditionalStatementsAndLoops-MoreExercise/01.SortNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿int numberOne = int.Parse(Console.ReadLine());
+int numberTwo = int.Parse(Console.ReadLine());
+int numberThree = int.Parse(Console.ReadLine());
+
+List<int> result = new List<int>
+{
+    numberOne,
+    numberTwo,
+    numberThree
+};
+
+result.Sort();
+
+for (int i = result.Count - 1; i >= 0; i--)
+{
+    Console.WriteLine(result[i]);
+}
---
+++ C# Fundamentals/BasicSyntax,ConditionalStatementsAndLoops-MoreExercise/03.GamingStore/Program.cs
@@ -0,0 +1,110 @@
+﻿double currentBalance = double.Parse(Console.ReadLine());
+string command;
+double gamePrice = 0;
+double totalSpent = 0;
+
+while ((command = Console.ReadLine()) != "Game Time")
+{
+    if (command == "OutFall 4")
+    {
+        gamePrice = 39.99;
+
+        if (currentBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+            continue;
+        }
+
+        currentBalance -= gamePrice;
+        totalSpent += gamePrice;
+
+        Console.WriteLine("Bought OutFall 4");
+    }
+    else if (command == "CS: OG")
+    {
+        gamePrice = 15.99;
+
+        if (currentBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+            continue;
+        }
+
+        currentBalance -= gamePrice;
+        totalSpent += gamePrice;
+
+        Console.WriteLine("Bought CS: OG");
+    }
+    else if (command == "Zplinter Zell")
+    {
+        gamePrice = 19.99;
+
+        if (currentBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+            continue;
+        }
+
+        currentBalance -= gamePrice;
+        totalSpent += gamePrice;
+
+        Console.WriteLine("Bought Zplinter Zell");
+    }
+    else if (command == "Honored 2")
+    {
+        gamePrice = 59.99;
+
+        if (currentBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+            continue;
+        }
+
+        currentBalance -= gamePrice;
+        totalSpent += gamePrice;
+
+        Console.WriteLine("Bought Honored 2");
+    }
+    else if (command == "RoverWatch")
+    {
+        gamePrice = 29.99;
+
+        if (currentBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+            continue;
+        }
+
+        currentBalance -= gamePrice;
+        totalSpent += gamePrice;
+
+        Console.WriteLine("Bought RoverWatch");
+    }
+    else if (command == "RoverWatch Origins Edition")
+    {
+        gamePrice = 39.99;
+
+        if (currentBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+            continue;
+        }
+
+        currentBalance -= gamePrice;
+        totalSpent += gamePrice;
+
+        Console.WriteLine("Bought RoverWatch Origins Edition");
+    }
+    else
+    {
+        Console.WriteLine("Not Found");
+    }
+
+    if (currentBalance == 0)
+    {
+        Console.WriteLine("Out of money!");
+        return;
+    }
+}
+
+Console.WriteLine($"Total spent: ${totalSpent:F2}. Remaining: ${currentBalance:F2}");
