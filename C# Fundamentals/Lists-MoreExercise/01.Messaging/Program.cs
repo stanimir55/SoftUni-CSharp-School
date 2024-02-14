71db996aa18dfe0eddb15448edee8c286da6048f
Stanimir <miro_2000s@abv.bg>
2024-02-14 22:07:32 +0200

    Added

---
+++ C# Fundamentals/Lists-MoreExercise/01.Messaging/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+List<char> strings = new List<char>();
+
+strings.AddRange(Console.ReadLine());
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    int sum = 0;
+
+    while (numbers[i] != 0)
+    {
+        sum += numbers[i] % 10;
+        numbers[i] /= 10;
+    }
+
+    int times = sum / strings.Count;
+    sum = sum - times * strings.Count;
+
+    Console.Write(strings[sum]);
+    strings.RemoveAt(sum);
+}
---
+++ C# Fundamentals/Lists-MoreExercise/05.DrumSet/Program.cs
@@ -0,0 +1,41 @@
+﻿double savings = double.Parse(Console.ReadLine());
+List<int> drumSet = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+List<int> drum = new List<int>(drumSet);
+
+int price = 0;
+string command;
+
+while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+{
+    int numbers = int.Parse(command);
+
+    for (int i = 0; i < drumSet.Count; i++)
+    {
+        drumSet[i] -= numbers;
+
+        price = drum[i] * 3;
+
+        if (drumSet[i] <= 0)
+        {
+            if (savings < price)
+            {
+                drumSet.Remove(drumSet[i]);
+                drum.Remove(drum[i]);
+                i--;
+            }
+            else
+            {
+                drumSet.Remove(drumSet[i]);
+                savings -= price;
+                drumSet.Insert(i, drum[i]);
+            }
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", drumSet));
+Console.WriteLine($"Gabsy has {savings:F2}lv.");
+
