92a33a39762355f7d615a40ff21c33d5a4e43e44
Stanimir <miro_2000s@abv.bg>
2024-03-13 22:44:35 +0200

    Added

---
+++ C# Fundamentals/RegularExpressions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+string pattern = @"\+359([- ])2\1[\d]{3}\1[\d]{4}\b";
+
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(input);
+
+string[] results = matches
+    .Cast<Match>()
+    .Select(a => a.Value.Trim())
+    .ToArray();
+
+Console.Write(string.Join(", ", results));
