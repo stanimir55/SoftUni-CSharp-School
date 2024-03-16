37d5b81c04ab9d44f01f14a8eadeb50c168f7fd2
Stanimir <miro_2000s@abv.bg>
2024-03-16 14:37:26 +0200

    Added

---
+++ C# Fundamentals/RegularExpressions-Exercise/04.StarEnigma/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+int messagesCount = int.Parse(Console.ReadLine());
+List<Message> messages = new List<Message>();
+
+string starPattern = @"[SsTtAaRr]";
+string messagesPattern = @"\@(?<planet>[A-Za-z]+)[^\@\-\!\:\>]*:(?<population>\d+)[^\@\-\!\:\>]*\!(?<type>A|D)\![^\@\-\!\:\>]*\-\>(?<soldires>\d+)";
+
+for (int i = 0; i < messagesCount; i++)
+{
+    string encryptMessage = Console.ReadLine();
+    int decryptionKey = Regex.Matches(encryptMessage, starPattern).Count;
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int k = 0; k < encryptMessage.Length; k++)
+    {
+        sb.Append((char)(encryptMessage[k] - decryptionKey));
+    }
+
+    string decryptedMessage = sb.ToString();
+    Match match = Regex.Match(decryptedMessage, messagesPattern);
+
+    if (!Regex.IsMatch(decryptedMessage, messagesPattern))
+    {
+        continue;
+    }
+
+
+    Message message = new Message();
+    message.PlanetName = match.Groups["planet"].Value;
+    message.Population = uint.Parse(match.Groups["population"].Value);
+    message.AttackType = match.Groups["type"].Value;
+    message.SoliderCount = uint.Parse(match.Groups["soldires"].Value);
+
+    messages.Add(message);
+}
+
+var filteredMessages = messages
+    .Where(m => m.AttackType == "A")
+    .OrderBy(m => m.PlanetName)
+    .ToList();
+
+Console.WriteLine($"Attacked planets: {filteredMessages.Count}");
+filteredMessages.ForEach(m => Console.WriteLine($"-> {m.PlanetName}"));
+
+filteredMessages = messages
+    .Where(m => m.AttackType == "D")
+    .OrderBy(m => m.PlanetName)
+    .ToList();
+
+Console.WriteLine($"Destroyed planets: {filteredMessages.Count}");
+filteredMessages.ForEach(m => Console.WriteLine($"-> {m.PlanetName}"));
+
+
+class Message
+{
+    public string PlanetName { get; set; }
+
+    public uint Population { get; set; }
+
+    public string AttackType { get; set; }
+
+    public uint SoliderCount { get; set; }
+
+}
---
+++ C# Fundamentals/RegularExpressions-Exercise/05.NetherRealms/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+string splitPattern = @" *,{1} *";
+string healthPattern = @"[^+\-*/.\d]";
+string damagePattern = @"((|-)\d+\.\d+|(|-)\d+)";
+string operationPattern = @"[\*\/]";
+
+string[] demons = Regex.Split(Console.ReadLine(), splitPattern);
+
+Regex healthRegex = new Regex(healthPattern);
+Regex damageRegex = new Regex(damagePattern);
+
+var demonData = new Dictionary<string, KeyValuePair<int, double>>(demons.Length - 1);
+
+foreach (var demon in demons.OrderBy(x => x))
+{
+    int helth = 0;
+    var sumChar = healthRegex.Matches(demon).ToArray();
+
+    foreach (var ch in sumChar)
+    {
+        helth += char.Parse(ch.Value);
+    }
+
+    double damage = 0;
+    var dmg = damageRegex.Matches(demon).ToArray();
+
+    foreach (var number in dmg)
+    {
+        damage += double.Parse(number.Value);
+    }
+
+    var mathSymbols = Regex.Matches(demon, operationPattern).ToArray();
+
+    foreach (var symbol in mathSymbols)
+    {
+        if (symbol.Value == "*")
+        {
+            damage *= 2;
+        }
+        else if (symbol.Value == "/")
+        {
+            damage /= 2;
+        }
+    }
+
+    demonData[demon] = new KeyValuePair<int, double>(helth, damage);
+}
+
+foreach (var demon in demonData)
+{
+    Console.WriteLine($"{demon.Key} - {demon.Value.Key} health, {demon.Value.Value:F2} damage");
+}
