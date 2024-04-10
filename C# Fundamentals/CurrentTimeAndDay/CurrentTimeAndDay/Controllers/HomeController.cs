cbb9ec64afec285b7d48e70b86762aa43b9799f6
Stanimir <miro_2000s@abv.bg>
2024-04-10 22:33:29 +0300

    Added

---
+++ C# Fundamentals/CurrentTimeAndDay/CurrentTimeAndDay/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CurrentTimeAndDay.Models;
+
+namespace CurrentTimeAndDay.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Asia()
+    {
+        return View();
+    }
+
+    public IActionResult Africa()
+    {
+        return View();
+    }
+
+    public IActionResult NorthAmerica()
+    {
+        return View();
+    }
+
+    public IActionResult SouthAmerica()
+    {
+        return View();
+    }
+
+    public IActionResult Europe()
+    {
+        return View();
+    }
+
+    public IActionResult AustraliaAndOceania()
+    {
+        return View();
+    }
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
