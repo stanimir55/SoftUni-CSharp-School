6249521e744f22a7654062a1e56fba9031a6a146
Stanimir <miro_2000s@abv.bg>
2024-05-21 22:31:54 +0300

    Added

---
+++ C# Advanced/C# Advanced/SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,13 @@
+﻿int linesCount = int.Parse(Console.ReadLine());
+HashSet<string> usernames = new HashSet<string>();
+
+for (int i = 0; i < linesCount; i++)
+{
+    string username = Console.ReadLine();
+    usernames.Add(username);
+}
+
+foreach (var username in usernames)
+{
+    Console.WriteLine(username);
+}
---
+++ C# Advanced/C# Advanced/SetsAndDictionariesAdvanced-Exercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] lenghtOfSets = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int n = lenghtOfSets[0];
+int m = lenghtOfSets[1];
+
+HashSet<int> firstSet = new HashSet<int>();
+HashSet<int> secondSet = new HashSet<int>();
+
+for (int i = 0; i < n; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    firstSet.Add(currentNumber);
+}
+
+for (int i = 0; i < m; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    secondSet.Add(currentNumber);
+}
+
+Console.WriteLine(string.Join(" ", firstSet.Intersect(secondSet)));
