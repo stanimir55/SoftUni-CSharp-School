39a880f5f8451695f838868d5109879851a5e94a
Stanimir <miro_2000s@abv.bg>
2024-06-06 17:01:19 +0300

    Added

---
+++ C# Advanced/C# Advanced/Generics-Lab/02.ArrayCreator/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ",ArrayCreator.Create(20, "T")));
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/Generics-Lab/03.GenericScale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>(10, 10);
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}
